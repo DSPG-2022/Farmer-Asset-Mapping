334ec8976854974772c7409739c1b01843e1fdb3
CoryRoth <cgroth@iastate.edu>
2022-06-16 11:46:23 -0500

    Update Cory_CropSelectionTest.R

--- Data Exploration/R Projects/Cory_CropSelectionTest.R
+++ Data Exploration/R Projects/Cory_CropSelectionTest.R
@@ -1,7 +1,8 @@
 library(ggplot2)
 library(FedData)
 library(raster)
-
+library(readxl)
+library(dplyr)
 r <- raster(ncol=10, nrow=10)
 
 
@@ -16,3 +17,23 @@ xmax(r) <- -93.83524145838685
 # 42.0781790407238, -93.83506979701508
 
 Test.area <- get_ssurgo(template = r,label = "Crop_SelectionTest")
+
+##selecting the important Tables
+Test.areaTabular <- Test.area$tabular
+Componet <- Test.areaTabular$component
+chorizon <- Test.areaTabular$chorizon
+mapunit <- Test.areaTabular$mapunit
+legend <- Test.areaTabular$legend
+
+
+
+chorzonSim <-select(chorizon, cokey,chkey,hzdept.l,hzdept.r,hzdept.h, ph1to1h2o.l,ph1to1h2o.r,ph1to1h2o.h) %>%
+  mutate(depth = hzdept.r /2.54)
+chorzonSim <-chorzonSim%>%
+  mutate(depthLevel = ifelse(depth <12,"0-12",ifelse(depth<24, "12-24",ifelse(depth<36,'24-36',"36+"))))
+
+
+chorzonSim <-chorzonSim %>%
+  group_by(cokey, depthLevel) %>%
+  mutate(pH_average = mean(ph1to1h2o.r, na.rm=TRUE))
+CropData <- read_excel("Crop-Info_Farmer Asset Mapping.xlsx")
