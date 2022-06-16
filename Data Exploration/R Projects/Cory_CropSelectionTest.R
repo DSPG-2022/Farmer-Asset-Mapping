6580cfe02fb3630f4c18ef4e9f052640835c72da
CoryRoth <cgroth@iastate.edu>
2022-06-16 11:57:56 -0500

    Update Cory_CropSelectionTest.R

--- Data Exploration/R Projects/Cory_CropSelectionTest.R
+++ Data Exploration/R Projects/Cory_CropSelectionTest.R
@@ -26,14 +26,30 @@ mapunit <- Test.areaTabular$mapunit
 legend <- Test.areaTabular$legend
 
 
-
+##for chorizon Select only important columns
+  ##as of now only depth and ph are important, probably need to add some
 chorzonSim <-select(chorizon, cokey,chkey,hzdept.l,hzdept.r,hzdept.h, ph1to1h2o.l,ph1to1h2o.r,ph1to1h2o.h) %>%
-  mutate(depth = hzdept.r /2.54)
+  mutate(depth = hzdept.r /2.54) ##set to Inches
+
+##Group Depths together
 chorzonSim <-chorzonSim%>%
   mutate(depthLevel = ifelse(depth <12,"0-12",ifelse(depth<24, "12-24",ifelse(depth<36,'24-36',"36+"))))
 
 
+##Find Average pH by Depth Level
 chorzonSim <-chorzonSim %>%
   group_by(cokey, depthLevel) %>%
   mutate(pH_average = mean(ph1to1h2o.r, na.rm=TRUE))
+
+##simplifying Componet
+ComponetSim <-select(Componet, cokey,mukey,slope.l,slope.r,slope.l,geomdesc,elev.l,elev.r,elev.h,taxclname,taxpartsize)
+
+##merges Them
+Overall <- merge(ComponetSim,mapunit, by.x = "mukey", by.y = "mukey",all.x = TRUE, all.y =TRUE)
+Overall <- merge(Overall,legend, by.x = "lkey", by.y = "lkey",all.x = TRUE, all.y =TRUE)
+Overall <- merge(Overall,chorzonSim, by.x = "cokey", by.y = "cokey",all.x = TRUE, all.y =TRUE)
+
+write.csv(Overall,"6_16_CropSelectionTest.csv")
+
+##Crop Info
 CropData <- read_excel("Crop-Info_Farmer Asset Mapping.xlsx")
