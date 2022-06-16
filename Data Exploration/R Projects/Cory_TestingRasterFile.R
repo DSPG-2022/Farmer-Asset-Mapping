f26239ac82a8ef6a5f2bc19775d25794375251a2
CoryRoth <cgroth@iastate.edu>
2022-06-16 09:28:57 -0500

    RasterFileCreation

---
+++ Data Exploration/R Projects/Cory_TestingRasterFile.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(FedData)
+require(rgdal)
+
+r <- raster(ncol=10, nrow=10)
+
+xmax(r) <- 0
+xmin(r) <- 0
+ymax(r) <- 0
+ymin(r) <- 
+# 42.038534290775225, -93.48364437815002 Upper left
+# 42.037004368587496, -93.42596615723883 Upper Right
+# 41.999254629022786, -93.47883785974075 Lower Left
+# 41.99721346419466, -93.4273394482129 Lower Right
+  
