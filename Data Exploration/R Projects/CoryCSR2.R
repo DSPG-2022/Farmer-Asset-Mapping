95d2a634cee86e4e363a224e48c23684c5453329
CoryRoth <cgroth@iastate.edu>
2022-06-30 10:49:59 -0500

    Update CoryCSR2.R

--- Data Exploration/R Projects/CoryCSR2.R
+++ Data Exploration/R Projects/CoryCSR2.R
@@ -15,9 +15,9 @@ Space <- SSURGO.areas$spatial
 #comonth$pond3frercl
 #comonth$ponddurcl
 #componet$slope.r
-#componet$erocl
+#componet$erocl - how much topsoil has been lost
 #componet$localphase
-#componettfact
+#componettfact - erosion tolerance
 #componet$wei
 #componet$taxorder
 #componet$compname
@@ -30,15 +30,39 @@ muaggart <-IA001.Tabular$muaggatt
 
 mapunit <-IA001.Tabular$mapunit
 
+comonth <- IA001.Tabular$comonth %>% filter(month=="May")
+
 Overall<- merge(muaggart,componet,by = "mukey", all = TRUE)
 Overall<- merge(Overall,mapunit,by = "mukey", all = TRUE)
+Overall<- merge(Overall,comonth,by = "cokey", all = TRUE)
+
+
+chorizon <- IA001.Tabular$chorizon
+
+coerosionacc <- IA001.Tabular$coerosionacc %>%
+  filter(rvindicator=="Yes")
+Overall<- merge(Overall,coerosionacc,by = "cokey", all = TRUE)
+
+
+
+chtexturegrp <-IA001.Tabular$chtexturegrp %>%
+  filter(rvindicator == "Yes")
+chorizon <- merge(chorizon,chtexturegrp, by = "chkey", all.x=TRUE)
+
+ ##set to Inche
+chorzonSim <-chorizon%>%
+  mutate(depth = hzdept.r /2.54)%>%
+  mutate(depthLevel = ifelse(depth <12,"0-12",ifelse(depth<24, "12-24",ifelse(depth<36,'24-36',"36+"))))
+chorzonSim2<- chorzonSim%>%
+  filter(depthLevel=="0-12")%>%
+  group_by(depthLevel)%>%
+  summarise(ksat = mean(ksat.r,na.rm=TRUE),awc = mean(awc.r, na.rm=TRUE),caco3 = mean(caco3.r,na.rm=TRUE),gypsum= mean(gypsum.r,na.rm=TRUE),sar =mean(sar.r,na.rm=TRUE),ec = mean(ec.r,na.rm=TRUE),cec7= mean(cec7.r,na.rm=TRUE),textdes = texdesc, ph = mean(ph1to1h2o.r,na.rm=TRUE),ph_l = min(ph1to1h2o.l,na.rm=TRUE),ph_h= max(ph1to1h2o.h,na.rm=TRUE))
+
 
 Overall <- Overall %>%
   filter(majcompflag=="Yes")
 Simple <- Overall %>%
-  select(mukey,cokey,muname = muname.x,taxorder,compname,slope.r,slopegradwta, slope.l,slope.h,localphase,erocl,hydgrp,soilslippot,drainagecl,drclassdcd,niccdcd,aws025wta,aws050wta)
+  select(mukey,cokey,muname = muname.x,taxorder,compname,slope.r,slopegradwta, slope.l,slope.h,localphase,erokind,erocl,tfact,wei,niccdcd,hydgrp,soilslippot,drainagecl,drclassdcd,niccdcd,aws025wta,aws050wta, flodfreqcl,floddurcl,pondfreqcl,pondfreqprs,flodfreqdcd,flodfreqmax,pondfreqprs,iacornsr)
 
-cotaxfmmin <- IA001.Tabular$cotaxfmmin
-cotaxmoistcl <- IA001.Tabular$cotaxmoistcl #taxmoistcl
-cotxfmotherv <-IA001.Tabular$cotxfmother #taxfamother
+
 
