5fc9058d34385198522e665fc68a2f69dea37bac
Bailey Hanson <bahanson@iastate.edu>
2022-07-20 10:24:41 -0500

    testing php and r

---
+++ docs/php_r_test/php_test.php
@@ -0,0 +1,20 @@
+<?php
+ 
+echo "<form action="https://r-bloggers.com/phplist/?p=subscribe&id=1" method="post" target="popupwindow">";
+echo "Number values to generate: <input type='text' name='N' />";
+echo "<input type='submit' />";
+echo "</form>";
+ 
+if(isset($_GET['N']))
+{
+  $N = $_GET['N'];
+ 
+  // execute R script from shell
+  // this will save a plot at temp.png to the filesystem
+  exec("Rscript my_rscript.R $N");
+ 
+  // return image tag
+  $nocache = rand();
+  echo("<img src='temp.png?$nocache' />");
+}
+?>	
